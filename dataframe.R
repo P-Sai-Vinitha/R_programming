979e07d070a36739a368c4ec6d26debc89696d67
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2023-03-21 20:31:23 +0530

    Add files via upload

--- dataframe.R
+++ dataframe.R
@@ -1,2 +1,14 @@
-a<-data.frame(ID=c(1:4),clothingbrands=c("h&m","zara","forever21","mango"),footwarebrands=c("mochi","metro","gucci","jimmy choo"),offers=c("10percent","40percent,"50percent","20percent"))
+a<-data.frame(ID=c(1:4),clothingbrands=c("h&m","zara","forever21","mango"),footwarebrands=c("mochi","metro","gucci","jimmy choo"),offers=c(TRUE,TRUE,TRUE,FALSE))
 print(a)
+
+
+b<-data.frame(foodcourt=c("KFC","dominos","pizza hut","cream stone"))
+
+c<-cbind(a,b)
+print(c)
+
+
+
+e<-data.frame(ID=c(1:4),clothingbrands=c("h&m","zara","forever21","mango"),footwarebrands=c("mochi","metro","gucci","jimmy choo"),offers=c(TRUE,TRUE,TRUE,FALSE))
+f<-rbind(a,e)
+print(f)
