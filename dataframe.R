9191e552374f65dcf948251a80441da0ba20cbda
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2023-03-21 11:21:41 +0530

    Add files via upload

---
+++ dataframe.R
@@ -0,0 +1,2 @@
+a<-data.frame(ID=c(1:4),clothingbrands=c("h&m","zara","forever21","mango"),footwarebrands=c("mochi","metro","gucci","jimmy choo"),offers=c("10percent","40percent,"50percent","20percent"))
+print(a)
